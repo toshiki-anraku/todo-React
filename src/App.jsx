adc5d08ae4a0eeab83c00ea4aca7c35a4cebe62a
anchan <62826339+toshiki-anraku@users.noreply.github.com>
2022-05-02 16:51:52 +0900

    html追加

--- src/App.jsx
+++ src/App.jsx
@@ -2,5 +2,38 @@ import React from "react";
 import "./styles.css";
 
 export const App = () => {
-  return <div></div>;
+  return (
+    <>
+      <div>
+        <input placeholder="TODOを入力" />
+        <button>追加</button>
+      </div>
+      <div>
+        <p>未完了のTODO</p>
+        <ul>
+          <div>
+            <li>ああああああああ</li>
+            <button>完了</button>
+            <button>削除</button>
+          </div>
+        </ul>
+        <ul>
+          <div>
+            <li>いいいいいいい</li>
+            <button>完了</button>
+            <button>削除</button>
+          </div>
+        </ul>
+      </div>
+      <div>
+        <p>完了のTODO</p>
+        <ul>
+          <div>
+            <li>うううううううう</li>
+            <button>戻す</button>
+          </div>
+        </ul>
+      </div>
+    </>
+  );
 };
